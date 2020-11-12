c83df4fcd4ea1caf95707b33c2be1c2c6273bbfa
DbIm-AZOV <74107512+DbIm-AZOV@users.noreply.github.com>
2020-11-12 23:42:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+//координаты ячеек
+let cell= document.getElementsByClassName("cell")
+let x= 1;
+let y= 1;
+for (let i=0 ; i<cell.length; i++){
+    cell[i].setAttribute('posX', x);
+    cell[i].setAttribute('posY', y);
+    x++;
+    if (x==12) {
+        x=1;
+        y++}    
+}
 
+// создание ящика
+function createbox(){
+    function generateBox(){
+        let posX= Math.round(Math.random()*(11-1)+1);
+        let posY= Math.round(Math.random()*(8-1)+1);
+        return[posX, posY];
+    }
+    let coordinates=generateBox();
+    let box=[document.querySelector(`[posX="` + coordinates[0]+`"][posY = "` +
+    coordinates[1]+`"]`)];
+    box[0].classList.add('box');
+}
 
+createbox();
+createbox();
+createbox();
 
-let cell= document.getElementById("qwer")
-alert(cell);
